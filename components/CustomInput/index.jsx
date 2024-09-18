9e8d44f3f710a8874445d005e49e52f25b8e2bf1
yashashvitaank <yashashvitaank20@gmail.com>
2024-09-18 15:43:23 +0530

    text color corrected

--- components/CustomInput/index.jsx
+++ components/CustomInput/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CustomInput({placeholder}) {
   return (
 <div className="w-full">
-  <input type="text" className="w-full relative ring-0 bg-white outline-none border-2 border-zinc-300 text-white placeholder-violet-800 text-sm rounded-lg focus:ring-violet-400  focus:border-violet-400 block p-2.5 checked:bg-emerald-500" placeholder={placeholder} />
+  <input type="text" className="w-full relative ring-0 bg-white outline-none border-2 border-zinc-300 text-black placeholder-violet-800 text-sm rounded-lg focus:ring-violet-400  focus:border-violet-400 block p-2.5 checked:bg-emerald-500" placeholder={placeholder} />
 </div>
   )
 }
